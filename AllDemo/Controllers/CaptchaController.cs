8d1d5d0118ab8691ae8994212a4b0b3e4fd64a3c
Sirsananda <mohanty.sirsananda@gmail.com>
2025-08-05 11:51:57 +0530

    reCAPTCHA added successfully

---
+++ AllDemo/Controllers/CaptchaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace AllDemo.Controllers
+{
+    public class CaptchaController : Controller
+    {
+        private readonly Random _random = new();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GenerateCaptcha()
+        {
+            int num1 = _random.Next(10, 100);
+            int num2 = _random.Next(1, num1); //confirm num1>num2
+            string captchaText = $"{num1}-{num2}";
+            int answer = num1 - num2;
+            HttpContext.Session.SetInt32("CaptchaAnswer", answer);
+            return Json(new { captcha = captchaText });
+        }
+
+        [HttpPost]
+        public JsonResult ValidateRecaptcha(string input)
+        {
+            int? correctAnswer = HttpContext.Session.GetInt32("CaptchaAnswer");
+
+            return Json(new { success = Convert.ToInt32(input)  == correctAnswer ? true : false });
+
+        }
+
+    }
+
+}
