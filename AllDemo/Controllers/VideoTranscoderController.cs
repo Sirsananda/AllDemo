775b5f7a0a18115bf00cdb20604a41f6e940b565
Sirsananda <mohanty.sirsananda@gmail.com>
2026-06-05 11:31:51 +0530

    video quality options uses using videotranscoder

---
+++ AllDemo/Controllers/VideoTranscoderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllDemo.Controllers
+{
+    public class VideoTranscoderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>  Upload(IFormFile file)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
